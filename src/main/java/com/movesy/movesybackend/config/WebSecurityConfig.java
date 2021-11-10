d9f0c86342af8f760948d406c3172681d1c2a5ca
Pinter Tamas <pintertamas99@gmail.com>
2021-11-10 09:58:03 +0100

    permission handling is done with the wrong endpoints

--- src/main/java/com/movesy/movesybackend/config/WebSecurityConfig.java
+++ src/main/java/com/movesy/movesybackend/config/WebSecurityConfig.java
@@ -31,9 +31,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // configure AuthenticationManager so that it knows from where to load
-        // user for matching credentials
-        // Use BCryptPasswordEncoder
         auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
@@ -50,18 +47,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // We don't need CSRF for this example
         httpSecurity.csrf().disable()
-                // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate", "/register", "/diary").permitAll().
-                // all other requests need to be authenticated
-                        anyRequest().authenticated().and().
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
+                .authorizeRequests().antMatchers("/authenticate", "/register", "/diary").permitAll()
+                .antMatchers(
+                        "/user/edit/{userID}",
+                        "/package/edit/{packageID}", "/package/delete/{packageID}", "/package/user/{userID}",
+                        "/review/create", "/review/edit/{reviewID}", "/review/delete/{reviewID}",
+                        "/offer/{packageID}", "/offer/accept/{quoteID}", "/offer/reject/{quoteID}")
+                .access("hasRole('ADMIN') or hasRole('USER')")
+                .antMatchers(
+                        "/package/all", "/package/transporter/{userID}",
+                        "offer/create{packageID}", "/offer/edit/{offerID}")
+                .access("hasRole('ADMIN') or hasRole('TRANSPORTER')")
+                .antMatchers(
+                        "/package/{packageID}", "/review/{packageID}",
+                        "/review/transporter/{transporterID}")
+                .access("hasRole('ADMIN') or hasRole('USER') or hasRole('TRANSPORTER')")
+                .anyRequest().hasRole("ADMIN").
+                        and().
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        // Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
