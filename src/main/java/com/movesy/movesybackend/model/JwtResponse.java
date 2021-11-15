0c831ea14e011fc21ae01ddd41a2560cae445151
Pinter Tamas <pintertamas99@gmail.com>
2021-11-15 14:25:57 +0100

    the authentication response contains the authenticated user too

--- src/main/java/com/movesy/movesybackend/model/JwtResponse.java
+++ src/main/java/com/movesy/movesybackend/model/JwtResponse.java
@@ -8,12 +8,18 @@ public class JwtResponse implements Serializable {
     @Serial
     private static final long serialVersionUID = -8091879091924046844L;
     private final String jwtToken;
+    private final User user;
 
     public JwtResponse(String jwtToken, User user) {
         this.jwtToken = jwtToken;
+        this.user = user;
     }
 
     public String getToken() {
         return this.jwtToken;
+    }
+
+    public User getUser() {
+        return this.user;
     }
 }
