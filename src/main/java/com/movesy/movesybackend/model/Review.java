b06ba991f0b73f9954ae513f4b437fcd33e6f671
ETomi24 <tamas.ernyei@gmail.com>
2021-11-15 18:56:39 +0100

    validation

--- src/main/java/com/movesy/movesybackend/model/Review.java
+++ src/main/java/com/movesy/movesybackend/model/Review.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Setter
@@ -20,8 +21,15 @@ public class Review {
 
     private String transporterID;
     private String packageID;
+    @NotBlank(message = "Username cannot be empty at Review")
+    @NotNull(message = "Username cannot be empty at Review")
+    @Pattern(regexp = "/^[a-zA-Z]{5,20}$/",message = "Username length must be in 5 to 20 range")
     private String customerUsername;
+    @NotNull(message = "You must give a time for Review")
     private Date time;
+    @Min(value = 0 , message = "The rating of an offer is smaller than 0")
+    @Max(value = 5  , message = "The rating of an offer is greater than 5")
     private int rating;
+    @Pattern(regexp = "/^[a-zA-Z]{0,5000}$/",message = "Description length must be in 0 to 5000 range")
     private String description;
 }
