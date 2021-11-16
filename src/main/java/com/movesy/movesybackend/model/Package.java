7649adbca2b4d69fca5ac1f233680f27c27595c9
Tomi <pintertamas99@gmail.com>
2021-11-16 12:17:57 +0100

    package verification annotation update

--- src/main/java/com/movesy/movesybackend/model/Package.java
+++ src/main/java/com/movesy/movesybackend/model/Package.java
@@ -7,10 +7,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Setter
@@ -24,8 +21,7 @@ public class Package {
 
     @NotNull(message = "Name cannot be null")
     @NotBlank(message = "Name cannot be null")
-    @Min(0)
-    @Max(20)
+    @Pattern(regexp = "^[a-zA-Z]{5,30}$")
     private String name;
 
     @NotNull(message = "UserID cannot be null")
