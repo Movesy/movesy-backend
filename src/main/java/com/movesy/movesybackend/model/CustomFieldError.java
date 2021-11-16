fec1924cb820c6d611ba08dbf459cbe4412cbcea
Tomi <pintertamas99@gmail.com>
2021-11-16 19:19:14 +0100

    validation now works properly changed every pattern so the correct regexes will not be denied

--- src/main/java/com/movesy/movesybackend/model/CustomFieldError.java
+++ src/main/java/com/movesy/movesybackend/model/CustomFieldError.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.ArrayList;
 import java.util.List;
