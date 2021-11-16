fcc09bf3830cf572e82e6a20687b8f08ff6d6560
Tomi <pintertamas99@gmail.com>
2021-11-16 12:44:35 +0100

    JwtResponse verification annotation update

--- src/main/java/com/movesy/movesybackend/model/JwtResponse.java
+++ src/main/java/com/movesy/movesybackend/model/JwtResponse.java
@@ -1,25 +1,23 @@
 package com.movesy.movesybackend.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serial;
 import java.io.Serializable;
 
+@RequiredArgsConstructor
+@Getter
 public class JwtResponse implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -8091879091924046844L;
+    @NotNull(message = "JwtToken should not be null")
+    @NotBlank(message = "JwtToken should not be blank")
     private final String jwtToken;
+
+    @NotNull(message = "User should not be null")
     private final User user;
-
-    public JwtResponse(String jwtToken, User user) {
-        this.jwtToken = jwtToken;
-        this.user = user;
-    }
-
-    public String getToken() {
-        return this.jwtToken;
-    }
-
-    public User getUser() {
-        return this.user;
-    }
 }
